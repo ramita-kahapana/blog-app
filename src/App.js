f7ad323f3ef5574ef5ad235a40612617ade83128
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-09 15:53:27 +0700

    edit-App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import routes from './config/routes'
-
+import withHelmet from './utils/withHelmet'
+import NavBar from './components/NavBar'
+import GlobalStyle from './components/GlobalStyle'
 function App () {
   return (
-    <Router>
-      <Suspense fallback='...loading'>
-        <Switch>
-          {Object.keys(routes).map(routeKey => (
-            <Route key={routeKey} {...routes[routeKey]} />
-          ))}
-        </Switch>
-      </Suspense>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Suspense fallback='...loading'>
+          <NavBar />
+          <Switch>
+            {Object.keys(routes).map(routeKey => (
+              <Route key={routeKey} {...routes[routeKey]} />
+            ))}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   )
 }
 
-export default App
+export default withHelmet('Ramita | Blog App')(App)
