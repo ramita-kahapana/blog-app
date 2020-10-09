49cddade3f28d528e7b06dae36c47815386ec5a1
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-09 15:55:28 +0700

    edit-Story

--- src/pages/Story.js
+++ src/pages/Story.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import withHelmet from '../utils/withHelmet'
 
 function Story () {
   const { storyId } = useParams()
   return <div>Story page{storyId}</div>
 }
-export default Story
+export default withHelmet('Ramita|Story')(Story)
