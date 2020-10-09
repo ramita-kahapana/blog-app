fa152d4141fdf7717622d2179c6e49b628e32768
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-09 15:54:12 +0700

    new-GlobalStyle

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+html,body,#root{
+  font-family :'Kanit',sanserif;
+  height:100vh;
+  width:100vw;
+  font-size:20px;
+
+  scroll-behavior:smooth;
+}
+a{
+  text-decoration:none;
+  color:inherit;
+  &visited{
+    color:inherit;
+  }
+}
+`
+
+export default GlobalStyle
