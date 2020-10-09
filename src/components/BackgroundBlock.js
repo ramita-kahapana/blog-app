f00d3340bebdfc1b4c65348d06ba325146a42f2c
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-09 15:53:55 +0700

    new-Background

---
+++ src/components/BackgroundBlock.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const BackgroundImage = styled.div`
+  height: ${props => props.height}vh;
+  width: auto;
+  background-image: url(${props => props.src});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+
+  position: relative;
+`
+const BackgroundOverylay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+function BackgroundBlock ({ src, height = 100, children }) {
+  return (
+    <div>
+      <BackgroundImage src={src} height={height}>
+        <BackgroundOverylay>{children}</BackgroundOverylay>
+      </BackgroundImage>
+    </div>
+  )
+}
+export default BackgroundBlock
